4945ab0f040b24ccf80ed3c9853b4abc5d11ed2b
Inkorrect-Code <ntongprofessional@gmail.com>
2023-12-04 14:47:44 -0500

    finished inheritance project

---
+++ practice-for-ch-js-prototypal-inheritance-long-practice-main/practice-for-ch-js-prototypal-inheritance-long-practice-main/inherits.js
@@ -0,0 +1,11 @@
+//  going to be tested
+
+Function.prototype.inherits = function(SuperClass) {
+    function Surrogate() {};
+    Surrogate.prototype = SuperClass.prototype;
+    this.prototype = new Surrogate();
+    this.prototype.constructor = this;
+
+
+
+}
